743bb974e1cae945fd0b1a936e649d4e75947d74
alde103 <aldebaran_103@hotmail.com>
2023-04-06 21:21:28 -0600

    Deps updated

--- mix.exs
+++ mix.exs
@@ -25,9 +25,7 @@ defmodule TcpServer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ring_logger, "0.5.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ring_logger, "~> 0.6"}
     ]
   end
 end
