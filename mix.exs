75ae4ced3133ea5b6c3903ea2b25f0b6c0dbbe6a
alde103 <aldebaran_103@hotmail.com>
2025-03-14 22:53:27 -0600

    deps updated

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TcpServer.MixProject do
   def project do
     [
       app: :tcp_server,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
